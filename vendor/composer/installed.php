46220e7e6ade375ad3a72350a3de2789e0962529
TaylanYildirim <yildirimtyln@gmail.com>
2022-04-30 15:54:56 +0200

    asd

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '06db869c0ed511ea0c0d6b071ec2b45fdfe07564',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '06db869c0ed511ea0c0d6b071ec2b45fdfe07564',
+            'dev_requirement' => false,
+        ),
+    ),
+);
